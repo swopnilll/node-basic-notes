b94f4eab5d681d14a7ea8499e20b71d1ed9117b6
Swopnil Acharya <swopnilacharya@gmail.com>
2022-09-21 08:05:37 +0545

    express-0: Using get from express app to return JSON list

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
-// Allow the new endpoint to the server that allow the user to get more specific information.
-// In Our case, it will be a page of data in JSON format. 
+const express = require('express');
+const app = express();
 
-// We can make the translation data available through the url. 
+const fortunes = require('./fortunes.json');
 
-const http = require('http');
-const hostName = "localhost";
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    const { url } = req;
-
-    console.log(url);
-
-    if(url === "/translations"){
-        
-        const translations = {1: "One", 2: "Two", 3: "Three"};
-        
-        res.setHeader('Content-Type', 'application/json');
-
-        res.write(JSON.stringify(translations));
-        res.end();
-    }
-
-    res.end("welcome to Node server")
+app.get('/fortunes', (req, res) => {
+    res.json(fortunes)
 })
 
-server.listen(port, hostName, () => console.log("server is running"));
+app.listen(3000, 'localhost', () => console.log("server starting"));
